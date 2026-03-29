a02e1804fa86e16844ace23524dd95e76b19152e
anilahida <anilaa.hidaa@gmail.com>
2026-03-29 17:20:50 +0200

    Shtimi i fotos kadare.webp

---
+++ resources/views/authors/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menaxhimi i Autoreve</title>
+</head>
+<body>
+    <h1>Menaxhimi i Autoreve</h1>
+    <table border="1" style="width:100%; border-collapse: collapse;">
+    <thead>
+        <tr style="background-color: #f2f2f2;">
+            <th>ID</th>
+            <th>Foto</th> <th>Emri</th>
+            <th>Mbiemri</th>
+            <th>Biografia</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($authors as $author)
+        <tr>
+            <td>{{ $author->author_id }}</td>
+            
+            <td>
+    @if($author->foto_autori)
+        <img src="{{ asset('images/' . $author->foto_autori) }}" width="80" alt="Foto e autorit">
+    @else
+        <span>Nuk ka foto</span>
+    @endif
+</td>
+
+            <td>{{ $author->emri }}</td>
+            <td>{{ $author->mbiemri }}</td>
+            <td>{{ $author->biografia }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+use App\Http\Controllers\AuthorController;
+
+// Kjo rrugë lidh URL-në /authors me funksionin index në Controller
+Route::get('/authors', [AuthorController::class, 'index']);
