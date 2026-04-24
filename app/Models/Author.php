c66a79d0141df927d20c80aad51c740ff1f1170a
nealaahavolli <nealahavolli0@gmail.com>
2026-04-24 22:54:08 +0200

    Rregullimi i modelit Author dhe mbushja e databazes me Seeders

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -9,9 +9,6 @@ class Author extends Model
 {
     use HasFactory;
 
-    // Kjo i tregon Laravelit që Primary Key nuk është 'id', por 'author_id'
-    protected $primaryKey = 'author_id';
-
-    // Sigurohu që emri i tabelës është 'authors' (zakonisht është automatike, por po e shtojmë për siguri)
-    protected $table = 'authors';
+    // Kjo lejon Laravel-in te mbushe keto kolona permes Seeder-it
+    protected $fillable = ['emri', 'biografia'];
 }
